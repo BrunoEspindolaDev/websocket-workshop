66191d78c8df159b3f5acba77d4e6baa8dbb28c2
Bruno Espindola <brunoespindola.work@gmail.com>
2025-04-04 22:37:19 -0300

    style: remove borders

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,17 +1,17 @@
-import ChatBar from "./ChatBar";
-import ChatNavbar from "./ChatNavbar";
-import ChatMessages from "./ChatMessages";
-import ChatUserDialog from "./ChatUserDialog";
+import ChatBar from './ChatBar';
+import ChatNavbar from './ChatNavbar';
+import ChatMessages from './ChatMessages';
+import ChatUserDialog from './ChatUserDialog';
 
 const Chat = () => {
   return (
-    <div className="flex flex-col h-screen max-w-[600px] mx-auto border-x shadow-lg">
-     <ChatNavbar/>
-     <ChatMessages />
-     <ChatBar />
-     <ChatUserDialog/>
+    <div className="flex flex-col h-screen max-w-[600px] mx-auto shadow-lg">
+      <ChatNavbar />
+      <ChatMessages />
+      <ChatBar />
+      <ChatUserDialog />
     </div>
   );
-}
+};
 
 export default Chat;
